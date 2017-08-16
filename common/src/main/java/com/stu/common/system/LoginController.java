9b5f2cea67275a6e49a5859278c3ff29dd5fe61b
yangyunxiang161019 <yangyunxiang@rongcapital.cn>
2017-08-16 16:46:35 +0800

    / to /system

--- common/src/main/java/com/stu/common/system/LoginController.java
+++ common/src/main/java/com/stu/common/system/LoginController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
-    @RequestMapping("/")
+    @RequestMapping("/system")
     public String index(){
         return "/system/index";
     }
